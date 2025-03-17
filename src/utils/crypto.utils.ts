32f74be8c3fbf8e7cd522a11e880e1625669ae66
stoodkev <quentin.correa@gmail.com>
2025-03-17 08:37:50 +0800

    remove cryptojs namespace

--- src/utils/crypto.utils.ts
+++ src/utils/crypto.utils.ts
@@ -1,19 +1,19 @@
 import * as bs58 from 'bs58';
-import { RIPEMD160, SHA256, lib } from 'crypto-js';
+import { RIPEMD160, SHA256, enc, lib } from 'crypto-js';
 const sha256 = (input: any): Buffer => {
   if (typeof input !== 'string') {
     input = lib.WordArray.create(input);
   }
-  const hash = Buffer.from(SHA256(input).toString(CryptoJS.enc.Hex), 'hex');
+  const hash = Buffer.from(SHA256(input).toString(enc.Hex), 'hex');
   return hash;
 };
 
 const ripemd160 = (input: any) => {
   // return crypto.createHash('ripemd160').update(input).digest()
   if (typeof input !== 'string') {
-    input = CryptoJS.lib.WordArray.create(input);
+    input = lib.WordArray.create(input);
   }
-  const hash = Buffer.from(RIPEMD160(input).toString(CryptoJS.enc.Hex), 'hex');
+  const hash = Buffer.from(RIPEMD160(input).toString(enc.Hex), 'hex');
   return hash;
 };
 export const isWif = (privWif: string | Buffer): boolean => {
