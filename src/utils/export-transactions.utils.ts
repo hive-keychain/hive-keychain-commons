87924f47e9011f2b38bde2df535103333aa36110
carlodizon03 <carlodizon03@gmail.com>
2025-03-20 03:54:34 +0000

    dev(export-transactions): branch out from main

---
+++ src/utils/export-transactions.utils.ts
@@ -0,0 +1,3 @@
+//TODO: https://app.clickup.com/t/86erwkkcy
+// implement the export transactions based on the implementation from the extension
+// ref: https://github.com/hive-keychain/hive-keychain-extension/blob/8e64c924b939c3aead7a2b755c29613fd6ae2a09/src/popup/hive/utils/export-transactions.utils.ts#L4
