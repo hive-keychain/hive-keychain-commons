a3243707dd3421e6c6624c13ada38b4ef8c5afc0
stoodkev <quentin.correa@gmail.com>
2025-11-20 14:32:42 +0800

    Add pair_id for recurrent transfers

--- src/interfaces/keychain.ts
+++ src/interfaces/keychain.ts
@@ -289,6 +289,7 @@ export type RequestRecurrentTransfer = CommonRequestParams & {
   memo: string;
   recurrence: number;
   executions: number;
+  pair_id?: number;
 };
 
 export type RequestSwap = CommonRequestParams & {
